9ef02148e271c26b43cf18638d736d9e78134336
diouser007 <71636013+diouser007@users.noreply.github.com>
2024-08-02 15:30:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "19067052"))
 API_HASH = environ.get("API_HASH", "f2205657fd3b0b40e9a0b11aac366656")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7136473319:AAGL2SY_jSgIwXzqbbk0oT4S42Jyd8h6u-c")
 
-PICS = (environ.get('PICS', 'https://desileaks.in/wp-content/uploads/2024/05/cropped-logo-no-background.png')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://desileaks.in/wp-content/uploads/2024/08/5d0ac3e9-73d0-485f-901a-871bf6ea09cb.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1871899094 6866849202').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "baguthacha_bot") # without @
 PORT = environ.get("PORT", "8080")
